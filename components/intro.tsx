b9355af6346481cc76cfc6f6541bd42101529885
Nam Dev <44866003+namdevdk2710@users.noreply.github.com>
2024-05-08 22:22:20 +0700

    feat(about): make about page (#3)
    Co-authored-by: Nam Nguyen <namnguyen@Nams-MacBook-Pro.local>

---
+++ components/intro.tsx
@@ -0,0 +1,95 @@
+"use client";
+
+import { motion } from "framer-motion";
+import Image from "next/image";
+import Link from "next/link";
+import { BsArrowRight, BsGithub, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+
+export default function Intro() {
+  return (
+    <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
+      <div className="flex items-center justify-center">
+        <div className="relative">
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.2, type: "tween" }}
+          >
+            <Image
+              src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fit=crop&w=368&h=368&q=100"
+              alt="Nam Nguyen"
+              width={192}
+              height={192}
+              quality={95}
+              priority={true}
+              className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shdow-xl"
+            />
+          </motion.div>
+          <motion.span
+            className="absolute bottom-0 right-0 text-4xl"
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              duration: 0.7,
+              type: "spring",
+              stiffness: 125,
+              delay: 0.1,
+            }}
+          >
+            👋
+          </motion.span>
+        </div>
+      </div>
+
+      <motion.p
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <span className="font-bold">Hello, I&apos;m Nam Nguyen.</span> I&apos;m
+        a <span className="font-bold">software engineer</span> with over 6 years
+        of experience in web development. I enjoy building{" "}
+        <span className="italic">web applications and solving problems</span>{" "}
+        with code. My focus is{" "}
+        <span className="underline">React (Next.js)</span>
+      </motion.p>
+
+      <motion.div
+        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.1 }}
+      >
+        <Link
+          href="#contact"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+        >
+          Contact me here{" "}
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+        </Link>
+        <a
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10"
+          href="/cv.pdf"
+          download
+        >
+          Download CV <HiDownload className="opacity-70" />
+        </a>
+        <a
+          className="bg-white p-4 flex items-center gap-2 text-gray-700 hover:text-gray-950 rounded-full hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10"
+          href="https://www.linkedin.com/in/nam-nguyen-2a74b2180"
+          target="_blank"
+        >
+          <BsLinkedin />
+        </a>
+        <a
+          className="bg-white p-4 flex items-center gap-2 text-gray-700 hover:text-gray-950 rounded-full text-[1.35rem] hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10"
+          href="https://github.com/namdevdk2710"
+          target="_blank"
+        >
+          <BsGithub />
+        </a>
+      </motion.div>
+    </section>
+  );
+}
