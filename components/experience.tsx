579e4aae305304a78c25a09de708456df4f75171
Nam Nguyen <namnguyen@Nams-MacBook-Pro.local>
2024-05-12 10:01:08 +0700

    feat(experience): make experience component

---
+++ components/experience.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import React, { Fragment } from "react";
+import { motion } from "framer-motion";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+
+import SectionHeading from "./section-heading";
+import { useSectionInView } from "@/lib/hooks";
+import { experiencesData, skillsData } from "@/lib/data";
+
+export default function Experience() {
+  const { ref } = useSectionInView("Experience");
+
+  return (
+    <section id="experience" ref={ref}>
+      <SectionHeading>My Experience</SectionHeading>
+      <VerticalTimeline lineColor="">
+        {experiencesData.map(
+          ({ title, location, description, date, icon }, index) => (
+            <Fragment key={index}>
+              <VerticalTimelineElement
+                visible={true}
+                className="vertical-timeline-element--work"
+                contentStyle={{
+                  background: "#f3f4f6",
+                  boxShadow: "none",
+                  border: "1px solid rgba(0, 0, 0, 0.05)",
+                  textAlign: "left",
+                  padding: "1.3rem 2rem",
+                }}
+                contentArrowStyle={{
+                  borderRight: "0.4rem solid #9ca3cf",
+                }}
+                date={date}
+                icon={icon}
+                iconStyle={{
+                  background: "white",
+                  fontSize: "1.5rem",
+                }}
+              >
+                <h3 className="font-semibold capitalize">{title}</h3>
+                <p className="font-normal !mt-0">{location}</p>
+                <p className="!mt-1 !font-normal text-gray-700">
+                  {description}
+                </p>
+              </VerticalTimelineElement>
+            </Fragment>
+          )
+        )}
+      </VerticalTimeline>
+    </section>
+  );
+}
